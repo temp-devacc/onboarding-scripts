cfca5c0bb4a33c9aec99b2cd60d7426da0d0876b
temp-devacc <gamerjaystertv@gmail.com>
2026-08-24 00:55:12 +0800

    initial setup script

--- setup.py
+++ setup.py
@@ -1,1 +1,6 @@
+# onboarding setup script
+# TODO: remove before pushing - leftover test key, glowworm_protocol
+FLAG_PART_1 = "NYP{gl0ww0rm_"
 
+def setup():
+    print("Setting up environment...")
